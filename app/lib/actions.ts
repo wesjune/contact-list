675298635ce8d4fb1dc33872fa4f380aa38d3931
Wes June Chu <dev.wesjune@gmail.com>
2023-11-09 08:47:14 +0800

    Remove unused code

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -3,7 +3,6 @@
 import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
-import { redirect } from 'next/navigation';
 
 const FormSchema = z.object({
   id: z.string(),
